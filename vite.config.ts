9fde0a78786f917db6de4b6fb6a6e3a22b4b7420
5323894n-spec <5323894n@gmail.com>
2026-04-26 21:42:54 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    // Добавляем базовый путь для GitHub Pages
+    base: '/obr_tranz/', 
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
